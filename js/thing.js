246fa225aba9b90c085985f5d367450239edd3da
Andrej Tish <potahanoob@gmail.com>
2017-11-18 20:19:39 +0500

    Create thing.js

---
+++ js/thing.js
@@ -0,0 +1,15 @@
+	var meme = 0;
+	function a(){
+		var meme = 0
+		};
+	function d(){
+		if (meme > 1) {
+			a() 
+		};
+		if (meme == 1) { 
+			notyf.confirm('Okay! <a style="color:white;" href="https://discord.gg/PEW4wx9">Click here!</a>') 
+		} else { 
+			notyf.confirm('I\'m tilda#4778 (if you wanna join the Discord server, click again');
+			meme++
+		}
+	}
