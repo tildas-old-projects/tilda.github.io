9290755910a8f791bbf2a424f290656658d93769
Andrej Tish <potahanoob@gmail.com>
2017-11-18 20:24:51 +0500

    Update thing.js

--- js/thing.js
+++ js/thing.js
@@ -1,19 +1,3 @@
-var meme = 0;
-	function a(){
-		var meme = 0
-		};
-	function d(){
-		if (meme > 1) {
-			a() 
-		};
-		if (meme == 1) { 
-			notyf.confirm('Okay! <a style="color:white;" href="https://discord.gg/PEW4wx9">Click here!</a>') 
-		} else { 
-			notyf.confirm('I\'m tilda#4778 (if you wanna join the Discord server, click again');
-			meme++
-		}
-	}
-
 var loadDeferredStyles = function() {
         var addStylesNode = document.getElementById("deferred-styles");
         var replacement = document.createElement("div");
@@ -28,3 +12,15 @@ var loadDeferredStyles = function() {
 }
 
 var notyf = new Notyf()
+var meme = 0; 
+function a(){
+	var meme = 0
+};
+function d(){
+	if (meme > 1) { a() };
+	if (meme == 1) { notyf.confirm('Okay! <a style="color:white;" href="https://discord.gg/PEW4wx9">Click here!</a>') 
+		       } else { 
+			       notyf.confirm('I\'m tilda#4778 (if you wanna join the Discord server, click again');
+			       meme++
+		       }
+}
