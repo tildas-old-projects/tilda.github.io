f99d340b17f7848d2a93becef67e34850a8687bd
S Stewart <tilda@users.noreply.github.com>
2017-11-21 12:40:55 -0600

    js sucks

--- js/thing.js
+++ js/thing.js
@@ -2,14 +2,13 @@ var loadDeferredStyles = function() {
         var addStylesNode = document.getElementById("deferred-styles");
         var replacement = document.createElement("div");
         replacement.innerHTML = addStylesNode.textContent;
-        document.body.appendChild(replacement)
+        document.body.appendChild(replacement);
         addStylesNode.parentElement.removeChild(addStylesNode);
       };
       var raf = requestAnimationFrame || mozRequestAnimationFrame ||
           webkitRequestAnimationFrame || msRequestAnimationFrame;
-      if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-      else window.addEventListener('load', loadDeferredStyles);
-}
+      if (raf) { raf(function() { window.setTimeout(loadDeferredStyles, 0); }); }
+      else { window.addEventListener("load", loadDeferredStyles); }
 
 var notyf = new Notyf()
 var meme = 0; 
